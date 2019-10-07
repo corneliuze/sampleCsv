18865989fe55d2a2a3d3fa00e9524eb91d94d142
corneliuze <falaycornelius@gmail.com>
2019-10-07 23:55:59 +0100

    uses kotlin-csv to read csv files to list of maps

--- app/src/main/java/com/example/samplecsv/MainActivity.kt
+++ app/src/main/java/com/example/samplecsv/MainActivity.kt
@@ -10,11 +10,18 @@ import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import java.io.File
+import java.io.Reader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 class MainActivity : AppCompatActivity() {
-
+ private lateinit var mapsOfContacts : MutableList<Map<String, String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,20 +33,26 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         // TODO Auto-generated method stub
 
         when (requestCode) {
 
             7 ->
-
                 if (resultCode == Activity.RESULT_OK) {
-
                     val pathHolder = data!!.data!!.path
+                   csvReader().open(pathHolder!!){
+                       readAllAsSequence().forEach { row ->
+                          val map = mapOf(row[0] to row[1])
+                           mapsOfContacts.add(map)
+                       } }
 
-                    Toast.makeText(this@MainActivity, pathHolder, Toast.LENGTH_LONG).show()
+                   }
+
+
 
                 }
         }
     }
-}
+
