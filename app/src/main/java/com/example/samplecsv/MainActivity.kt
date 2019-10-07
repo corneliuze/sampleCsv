714ccada8571ccf3974473ba0483b20362732b69
corneliuze <falaycornelius@gmail.com>
2019-10-07 08:00:43 +0100

    origin master

--- app/src/main/java/com/example/samplecsv/MainActivity.kt
+++ app/src/main/java/com/example/samplecsv/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.samplecsv
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        open_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "*/*"
+            startActivityForResult(intent, 7)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // TODO Auto-generated method stub
+
+        when (requestCode) {
+
+            7 ->
+
+                if (resultCode == Activity.RESULT_OK) {
+
+                    val pathHolder = data!!.data!!.path
+
+                    Toast.makeText(this@MainActivity, pathHolder, Toast.LENGTH_LONG).show()
+
+                }
+        }
     }
 }
